48aee46f372386e38b45e361d034774d91ae304a
elchavez14 <chatnoircatmua@gmail.com>
2023-11-17 10:51:33 -0400

    Validaciones

--- Models/Debt.cs
+++ Models/Debt.cs
@@ -1,26 +1,38 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Collections.Generic;
 
 namespace kadila.Models;
 
 public partial class Debt
 {
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public ulong Id { get; set; }
 
+    [Display(Name = "Fecha Límite")]
+    [DataType(DataType.Date)]
     public DateOnly? FechaLimite { get; set; }
 
+    [Display(Name = "Fecha de Creación")]
+    [DataType(DataType.Date)]
     public DateOnly? FechaCreacion { get; set; }
 
+    [Range(1, uint.MaxValue, ErrorMessage = "El campo Monto debe ser mayor o igual a 1.")]
     public uint? Monto { get; set; }
 
+    [StringLength(50, ErrorMessage = "El campo Estado no puede tener más de 50 caracteres.")]
     public string? Estado { get; set; }
 
     public ulong ClienteId { get; set; }
 
+    [Display(Name = "Fecha de Creación")]
     public DateTime? CreatedAt { get; set; }
 
+    [Display(Name = "Fecha de Actualización")]
     public DateTime? UpdatedAt { get; set; }
 
+    [ForeignKey("ClienteId")]
     public virtual Customer Cliente { get; set; } = null!;
 
     public virtual ICollection<PaymentHistory> PaymentHistories { get; set; } = new List<PaymentHistory>();
--- Models/Sale.cs
+++ Models/Sale.cs
@@ -1,14 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace kadila.Models;
 
 public partial class Sale
 {
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public ulong Id { get; set; }
 
+    [Display(Name = "Fecha de Venta")]
+    [DataType(DataType.Date)]
     public DateOnly? FechaVenta { get; set; }
 
+    [StringLength(50, ErrorMessage = "El campo TipoVenta no puede tener más de 50 caracteres.")]
     public string? TipoVenta { get; set; }
 
     public ulong EmpleadoId { get; set; }
@@ -17,14 +23,19 @@ public partial class Sale
 
     public ulong DeudaId { get; set; }
 
+    [Display(Name = "Fecha de Creación")]
     public DateTime? CreatedAt { get; set; }
 
+    [Display(Name = "Fecha de Actualización")]
     public DateTime? UpdatedAt { get; set; }
 
+    [ForeignKey("ClienteId")]
     public virtual Customer Cliente { get; set; } = null!;
 
+    [ForeignKey("DeudaId")]
     public virtual Debt Deuda { get; set; } = null!;
 
+    [ForeignKey("EmpleadoId")]
     public virtual User Empleado { get; set; } = null!;
 
     public virtual ICollection<SaleDetail> SaleDetails { get; set; } = new List<SaleDetail>();

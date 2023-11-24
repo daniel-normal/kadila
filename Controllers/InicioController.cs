1ee8aabbcc16b91de8cb4058d0e56e3bd2a51b1c
Daniel Miranda <daniel-normal>
2023-11-24 11:38:45 -0400

    Diseño 6

--- Controllers/InicioController.cs
+++ Controllers/InicioController.cs
@@ -1,9 +1,31 @@
-﻿using Microsoft.AspNetCore.Mvc;
-
+﻿using kadila.Data;
+using Microsoft.AspNetCore.Mvc;
+using kadila.Models;
 namespace Inicio.Controllers
 {
     public class InicioController : Controller
     {
+        private readonly DotnetContext _context;
+        
+        public InicioController(DotnetContext context)
+        {
+            _context = context;
+        }
+        
+        [HttpPost]
+        public IActionResult Contact(Contact model)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Contacts.Add(model);
+                _context.SaveChanges();
+                string name = model.Name;
+                TempData["SuccessMessage"] = $"Felicidades {name}, tu solicitud se envió exitosamente.";
+                return RedirectToAction("Contact");
+            }
+            return View(model);
+        }
+        
         public IActionResult Index()
         {
             return View();

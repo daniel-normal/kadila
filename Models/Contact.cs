beb5f5e0aab3ed015cefede6af9c001359147f0e
Daniel Miranda <daniel-normal>
2023-11-24 10:37:42 -0400

    Diseño 5

---
+++ Models/Contact.cs
@@ -0,0 +1,16 @@
+﻿namespace kadila.Models;
+
+public class Contact
+{
+    public int Id { get; set; }
+    
+    public string NameLastName { get; set; }
+
+    public string Email { get; set; }
+    
+    public string Phone { get; set; }
+
+    public string Topic { get; set; }
+    
+    public string Message { get; set; }
+}
